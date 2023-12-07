ebfdbddf8317260b212de6e25a8de20c646c42c0
Rafael <rafael.rued4@gmail.com>
2023-12-06 22:40:19 -0300

    Guesslify

--- project/urls.py
+++ project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('apps.rooms.urls')),
     path('', include('apps.start.urls')),
     path('', include('apps.game.urls')),
